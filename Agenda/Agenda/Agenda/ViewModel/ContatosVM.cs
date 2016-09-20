0ab24bfaf753e2771d51eb2e72fbabe843d70ba8
Gabriel <gabriel.souzadias@hotmail.com>
2016-09-20 17:48:03 -0400

    Last

--- Agenda/Agenda/Agenda/ViewModel/ContatosVM.cs
+++ Agenda/Agenda/Agenda/ViewModel/ContatosVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,25 @@ using Xamarin.Forms;
 
 namespace Agenda.ViewModel
 {
-    class ContatosVM
+    class ContatosVM : ViewModelBase
     {
         public ICommand AddCommand { get; set; }
+        private ObservableCollection<Model.ContatoModel> _listaContatos;
+        private int codigo;
+        private string nome;
+        private string sobrenome;
+        private string apelido;
+        private string email;
+        private string telefone;
+
+        public ObservableCollection<Model.ContatoModel> ListaContatos { get { return _listaContatos; } set { _listaContatos = value; Notify("ListaContatos"); } }
+        public int Codigo { get { return codigo; } set { codigo = value; Notify("Codigo"); } }
+        public string Nome { get { return nome; } set { nome = value; Notify("Nome"); } }
+        public string Sobrenome { get { return sobrenome; } set { sobrenome = value; Notify("Sobrenome"); } }
+        public string Apelido { get { return apelido; } set { apelido = value; Notify("Apelido"); } }
+        public string Email { get { return email; } set { email = value; Notify("Email"); } }
+        public string Telefone { get { return telefone; } set { telefone = value; Notify("Telefone"); } }
+
 
         public ContatosVM()
         {
@@ -19,6 +36,7 @@ namespace Agenda.ViewModel
 
         public void NavigateToCadastro()
         {
+            _listaContatos = new ObservableCollection<Model.ContatoModel>();
             Agenda.App.Current.MainPage.Navigation.PushAsync(new View.CadastroView());
         }
 
---
+++ Agenda/Agenda/Agenda/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Notify(String property)
+        {
+            if(PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
