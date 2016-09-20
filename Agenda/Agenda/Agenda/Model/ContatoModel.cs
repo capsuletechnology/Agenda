16859108a8d984f412371ef5f6218ea20acf474c
Walisson Felipe <allysson_felipe@hotmail.com>
2016-09-20 14:57:48 -0400

    Implementação da DAO

---
+++ Agenda/Agenda/Agenda/Model/ContatoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Model
+{
+    public class ContatoModel
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Apelido { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+
+
+
+    }
+}
