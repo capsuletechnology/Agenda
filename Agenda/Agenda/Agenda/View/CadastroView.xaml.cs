bbdd9afaa7952a5cf53f2279d337b51f628252e9
Gabriel <gabriel.souzadias@hotmail.com>
2016-09-21 16:52:49 -0400

    i see no changes

--- Agenda/Agenda/Agenda/View/CadastroView.xaml.cs
+++ Agenda/Agenda/Agenda/View/CadastroView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace Agenda.View
 		public CadastroView ()
 		{
 			InitializeComponent ();
-            BindingContext = new ViewModel.CadastroVM();
+            BindingContext = new ViewModel.ContatosVM();
             this.Title = "Novo Contato";
             this.BackgroundColor = Color.FromHex("#ecf0f1");
 		}
--- Agenda/Agenda/Agenda/ViewModel/ContatosVM.cs
+++ Agenda/Agenda/Agenda/ViewModel/ContatosVM.cs
@@ -12,24 +12,54 @@ namespace Agenda.ViewModel
     class ContatosVM : ViewModelBase
     {
         public ICommand AddCommand { get; set; }
+        public ICommand CadastrarCommand { get; set; }
+        private int i = 0;
+        //  private List<Model.ContatoModel> _listaContatos;
+        private int codigo;
+        private string nome;
+        private string sobrenome;
+        private string apelido;
+        private string email;
+        private string telefone;
         private List<Model.ContatoModel> lista;
+        //  public List<Model.ContatoModel> ListaContatos { get { return _listaContatos; } set { _listaContatos = value; Notify("ListaContatos"); } }
+        public int Codigo { get { return codigo; } set { codigo = value; Notify("Codigo"); } }
+        public string Nome { get { return nome; } set { nome = value; Notify("Nome"); } }
+        public string Sobrenome { get { return sobrenome; } set { sobrenome = value; Notify("Sobrenome"); } }
+        public string Apelido { get { return apelido; } set { apelido = value; Notify("Apelido"); } }
+        public string Email { get { return email; } set { email = value; Notify("Email"); } }
+        public string Telefone { get { return telefone; } set { telefone = value; Notify("Telefone"); } }
+
         public List<Model.ContatoModel> Lista { get { return lista; } set { lista = value; Notify("Lista"); } }
 
         public ContatosVM()
         {
-            Lista = new List<Model.ContatoModel>();
+            List<Model.ContatoModel> minhaLista = new List<Model.ContatoModel>();
+            CadastrarCommand = new Command(Cadastrar);
             this.AddCommand = new Command(NavigateToCadastro);
 
             using (var dados = new DAO.ContatoDAO())
             {
                 Lista = dados.Lista();
             }
-            
+
         }
 
         public void NavigateToCadastro()
         {
             Agenda.App.Current.MainPage.Navigation.PushAsync(new View.CadastroView());
+        }
+
+        public void Cadastrar()
+        {
+            using (var dados = new DAO.ContatoDAO())
+            {
+                dados.Insert(new Model.ContatoModel { Codigo = this.Codigo, Nome = this.Nome, Sobrenome = this.Sobrenome, Apelido = this.Apelido, Email = this.Email, Telefone = this.telefone });
+                
+            }            
+            Agenda.App.Current.MainPage.Navigation.PushAsync(new View.ContatosView());
+
+
         }
     }
 }
